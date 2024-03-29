3784b6f096ec20beabe018ab37296c4b1f144294
Júlia Lamboglia <julia.conip@gmail.com>
2024-03-29 12:13:10 -0300

    Refactor: Ajuste na estruturação do código

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:airsoft_bomb/main.dart';
+import 'package:airsoft_bomb/ui/pages/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
